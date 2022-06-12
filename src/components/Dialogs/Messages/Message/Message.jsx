d0cb20c3a2054c305f6bf8a776620ba867754477
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2022-06-12 17:32:08 +0400

    users api

--- src/components/Dialogs/Messages/Message/Message.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import module from '../../Dialogs.module.css';
-
-export const Message = (props) => {
-  return <div className={module.message}>
-    <img src={props.img} alt="img" />
-    <p> {props.message}</p>
-  </div>;
-};
