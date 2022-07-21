44e31c7d77ee269c7a4f3a003f58bc1c8b87a8bc
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2022-07-21 12:20:28 +0400

    User

---
+++ src/components/Users/User.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import module from './Users.module.css';
+import userPhoto from '../../assets/images/user.png';
+import { NavLink } from 'react-router-dom';
+
+export const User = ({ user, followingInProgress, unfollow, follow }) => {
+  return (
+    <div className={module.user}>
+      <div className={module.userFollow}>
+        <NavLink to={`profile/${user.id}`}><img src={user.photos.small != null ? user.photos.small : userPhoto} alt="img" /></NavLink>
+        {user.followed === true
+          ? <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => { unfollow(user.id); }}>Unfollow</button>
+          : <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => { follow(user.id); }}>Follow</button>}
+      </div>
+      <div className={module.userInfo}>
+        <div className={module.userLeft}>
+          <div className={module.userName}>
+            <NavLink to={`profile/${user.id}`}>{user.name}</NavLink></div>
+          <div className={module.userStatus}>{user.status || "no status"}</div>
+        </div>
+        <div className={module.userRight}>
+          <div className={module.userCountry}>{'user.location.country'} </div>
+          <div className={module.userCity}>{'user.location.city'}</div>
+        </div>
+      </div>
+    </div>);
+};
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,32 +1,13 @@
 import React from 'react';
 import module from './Users.module.css';
-import userPhoto from '../../assets/images/user.png';
-import { NavLink } from 'react-router-dom';
 import { Pagination } from '../common/Pagination/Pagination';
+import { User } from './User';
 
 export const Users = ({ users, followingInProgress, unfollow, follow, totalPageCount, pageSize, currentPage, onPageChange, portionSize }) => {
   return (
     <div>
       <div className={module.users}>
-        {users.map(user => <div key={user.id} className={module.user}>
-          <div className={module.userFollow}>
-            <NavLink to={`profile/${user.id}`}><img src={user.photos.small != null ? user.photos.small : userPhoto} alt="img" /></NavLink>
-            {user.followed === true
-              ? <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => { unfollow(user.id); }}>Unfollow</button>
-              : <button disabled={followingInProgress.some(id => id === user.id)} onClick={() => { follow(user.id); }}>Follow</button>}
-          </div>
-          <div className={module.userInfo}>
-            <div className={module.userLeft}>
-              <div className={module.userName}>
-                <NavLink to={`profile/${user.id}`}>{user.name}</NavLink></div>
-              <div className={module.userStatus}>{user.status || "no status"}</div>
-            </div>
-            <div className={module.userRight}>
-              <div className={module.userCountry}>{'user.location.country'} </div>
-              <div className={module.userCity}>{'user.location.city'}</div>
-            </div>
-          </div>
-        </div>)
+        {users.map(user => <User key={user.id} user={user} followingInProgress={followingInProgress} follow={follow} unfollow={unfollow} />)
         }</div >
       <Pagination totalPageCount={totalPageCount} pageSize={pageSize} currentPage={currentPage} onPageChange={onPageChange} portionSize={portionSize} />
     </div>
