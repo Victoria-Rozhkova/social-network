997be2010af871f37f5076fa08a83c8281c48c17
Victoria Rozhkova <victoria.rozhkova@ro.ru>
2022-07-21 14:15:48 +0400

    React.lazy( )

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./components/HomePage/HomePage";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import { NotFound } from "./components/NotFound/NotFound";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import { LoginContainer } from "./components/Login/Login";
 import { connect, Provider } from "react-redux";
 import { initializeApp } from "./redux/appReduser";
 import { Preloader } from "./components/common/Preloader/Preloader";
 import { compose } from "redux";
 import store from "./redux/store-redux";
+
+const DialogsContainer = React.lazy(() =>
+  import("./components/Dialogs/DialogsContainer")
+);
+const ProfileContainer = React.lazy(() =>
+  import("./components/Profile/ProfileContainer")
+);
+const UsersContainer = React.lazy(() =>
+  import("./components/Users/UsersContainer")
+);
+const LoginContainer = React.lazy(() => import("./components/Login/Login"));
+const NotFound = React.lazy(() => import("./components/NotFound/NotFound"));
 
 function App(props) {
   useEffect(() => props.initializeApp());
@@ -25,18 +32,20 @@ function App(props) {
       <HeaderContainer />
       <NavbarContainer />
       <div className="app-wrapper-content">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/profile" element={<ProfileContainer />} />
-          <Route path="/users/profile/:id" element={<ProfileContainer />} />
-          <Route path="/dialogs/" element={<DialogsContainer />} />
-          <Route path="/users" element={<UsersContainer />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/login" element={<LoginContainer />} />
-          {/* <Route path="/news" element={<News />} />
+        <Suspense fallback={<Preloader />}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/profile" element={<ProfileContainer />} />
+            <Route path="/users/profile/:id" element={<ProfileContainer />} />
+            <Route path="/dialogs/" element={<DialogsContainer />} />
+            <Route path="/users" element={<UsersContainer />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/login" element={<LoginContainer />} />
+            {/* <Route path="/news" element={<News />} />
             <Route path="/music" element={<Music />} />
             <Route path="/settings" element={<Settings />} /> */}
-        </Routes>
+          </Routes>
+        </Suspense>
       </div>
     </div>
   );
